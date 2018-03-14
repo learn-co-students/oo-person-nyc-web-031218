363e9e4cce2eff3bb97ed1cba5d7919dddfa5894
harim1206 <harim1206@gmail.com>
2018-03-14 16:03:20 -0400

    complete work_out

--- lib/person.rb
+++ lib/person.rb
@@ -43,6 +43,31 @@ class Person
     "all about the benjamins"
   end
 
+  def take_bath
+    self.hygiene=(@hygiene += 4)
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+
+  end
+
+  def work_out
+    self.hygiene=(@hygiene -=3)
+    self.happiness=(@happiness +=2)
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness=(@happiness +=3)
+    friend.happiness=(friend.happiness +=3)
+    "Hi #{friend.name}. It's #{self.name}! How are you?"
+
+  end
+
+
+
+
+
+
+
 end
 
 # vicky = Person.new("Vicky")
