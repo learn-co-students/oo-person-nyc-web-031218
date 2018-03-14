b552f1d3c5fa5546e9126868869ca592ee0fa3c3
victoria-huang <vctrah@gmail.com>
2018-03-14 15:44:42 -0400

    add class and instance methods

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,52 @@
-# your code goes here
+require 'pry'
+class Person
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(num)
+    if num > 10
+      @happiness =10
+    elsif num < 0
+      @happiness = 0
+    else
+      @happiness = num
+    end
+  end
+
+  def hygiene=(num)
+    if num > 10
+      @hygiene =10
+    elsif num < 0
+      @hygiene = 0
+    else
+      @hygiene = num
+    end
+  end
+
+  def happy?
+    @happiness > 7
+  end
+
+  def clean?
+    @hygiene > 7
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    "all about the benjamins"
+  end
+
+end
+
+# vicky = Person.new("Vicky")
+#
+# binding.pry
+#
+# puts "a"
