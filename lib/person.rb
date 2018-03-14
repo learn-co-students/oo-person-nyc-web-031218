1c21b78ffbd7e1dcca09f67875dd1dc5b75dd503
github username <github email address>
2018-03-14 16:45:15 -0400

    second commit

--- lib/person.rb
+++ lib/person.rb
@@ -48,13 +48,38 @@ class Person
   end
 
   def take_bath
-    @hygiene += 4
+    self.hygiene += 4
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   def work_out
-    @hygiene -= 3
-    @happiness += 2
+    self.hygiene -= 3
+    self.happiness += 2
     "♪ another one bites the dust ♫"
   end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{@name}. How are you?"
+  end
+
+  def happiness_change(amt)
+    self.happiness += amt
+  end
+
+  def start_conversation(convo_person, convo_topic)
+    if convo_topic == "politics"
+      self.happiness_change(-2)
+      convo_person.happiness_change(-2)
+      "blah blah partisan blah lobbyist"
+    elsif convo_topic == "weather"
+      self.happiness_change(1)
+      convo_person.happiness_change(1)
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+  
 end
