e5396d7a66b2cb7c6d9d94dbb78991e4f95b1ee9
harim1206 <harim1206@gmail.com>
2018-03-14 16:47:15 -0400

    FINISHED

--- lib/person.rb
+++ lib/person.rb
@@ -44,25 +44,46 @@ class Person
   end
 
   def take_bath
-    self.hygiene=(@hygiene += 4)
+    self.hygiene += 4
     "♪ Rub-a-dub just relaxing in the tub ♫"
 
   end
 
   def work_out
-    self.hygiene=(@hygiene -=3)
-    self.happiness=(@happiness +=2)
+    self.hygiene -= 3
+    self.happiness += 2
     "♪ another one bites the dust ♫"
   end
 
+  # def happiness=(num)
+
   def call_friend(friend)
-    self.happiness=(@happiness +=3)
-    friend.happiness=(friend.happiness +=3)
-    "Hi #{friend.name}. It's #{self.name}! How are you?"
+    self.happiness += 3
+    # binding.pry
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
 
   end
 
+  def start_conversation(friend, topic)
 
+    if topic == "politics"
+      self.happiness -= 2
+      friend.happiness -= 2
+      "blah blah partisan blah lobbyist"
+
+    elsif topic == "weather"
+      self.happiness += 1
+      friend.happiness += 1
+      "blah blah sun blah rain"
+
+    else
+      "blah blah blah blah blah"
+
+    end
+
+
+  end
 
 
 
